1cc0568301484b125aff8a948660cf2b73f68a6b
blackshovon <blackshovon@gmail.com>
2021-01-22 01:10:11 +0600

    third task done

--- assignment.js
+++ assignment.js
@@ -7,8 +7,43 @@ function kilometerToMeter(kilometer) {
     return meter;
 }
 var result = kilometerToMeter(156);
-console.log(result)
 
 // Simple Budget Calculator
 
-function budgetCalculator
+function budgetCalculator(watch, mobile, laptop) {
+    var watchCost = watch * 50;
+    var mobileCost = mobile * 100;
+    var laptopCost = laptop * 500;
+    var totalCost = watchCost + mobileCost + laptopCost;
+    return totalCost
+}
+
+// simple hotel costing calculator
+
+function hotelCost(days) {
+    if (days <= 10) {
+        var cost = 10 * 100;
+    }
+    else if (days <= 20) {
+        var tenDaysCost = 10 * 100;
+        var remainingDays = 20 - days;
+        var remainingDaysCost = remainingDays * 80;
+        var cost = tenDaysCost + remainingDaysCost
+
+    }
+    else {
+        var tenDaysCost = 10 * 100;
+        var secondTenDaysCost = 10 * 80;
+        var remainingDays = days - 20;
+        var remainingDaysCost = remainingDays * 50;
+        cost = tenDaysCost + secondTenDaysCost + remainingDaysCost;
+    }
+    return cost;
+}
+
+
+// Mega friend finder or friend with a higher name length
+
+function megaFriend[Array]{
+    
+}
